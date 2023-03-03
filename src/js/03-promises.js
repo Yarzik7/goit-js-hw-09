6adce09b1a02109a7ea7a390f8fed3cdbb606e9a
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-03-03 22:49:13 +0200

    Виконано завдання 9.3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+console.log(form.elements);
+
+const onCreatePromises = event => {
+  event.preventDefault();
+
+  let {
+    delay: { value: delay },
+    step: { value: step },
+    amount: { value: amount },
+  } = event.currentTarget.elements;
+
+  delay = Number(delay);
+  step = Number(step);
+  amount = Number(amount);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) =>
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      )
+      .catch(({ position, delay }) =>
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+    delay += step;
   }
+};
+
+form.addEventListener('submit', onCreatePromises);
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => 
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      const promiseParameters = { position, delay };
+      shouldResolve ? resolve(promiseParameters) : reject(promiseParameters);
+  }, delay)
+ );
 }
